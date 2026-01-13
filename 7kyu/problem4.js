95a638304716c63c651cac57b51abb3ebd4824a4
NOBA235 <nanova556@gmail.com>
2026-01-13 21:37:31 +0530

    Applied different method for solving

--- 7kyu/problem4.js
+++ 7kyu/problem4.js
@@ -10,7 +10,27 @@
 
 //Complete the solution so that it returns the greatest sequence of five consecutive digits found within the number given. The number will be passed in as a string of only digits. It should return a five digit integer. The number passed may be as large as 1000 digits.
 
-/*--------------using window slice method-----------*/
+/*--------------using window sliding method-----------*/
+
+function solution(digits) {
+  let max = 0;
+  
+  // Iterate through the string, looking at each 5-digit window
+  for (let i = 0; i <= digits.length - 5; i++) {
+    // Extract 5-digit substring and convert to number
+    const num = parseInt(digits.substring(i, i + 5));
+    
+    // Update max if this number is larger
+    if (num > max) {
+      max = num;
+    }
+  }
+  
+  return max;
+}
+
+
+/*----------slice mmethod---------*/
 
 // This is the best solution for the kata
 function solution(digits) {
