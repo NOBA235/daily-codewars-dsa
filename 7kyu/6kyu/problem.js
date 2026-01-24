cad0a50892affb4c77224d6c043aaa015018ffe6
NOBA235 <nanova556@gmail.com>
2026-01-24 20:18:07 +0530

    Remove anchor url

---
+++ 7kyu/6kyu/problem.js
@@ -0,0 +1,17 @@
+
+/*JavaScript
+Rank Up
+Train
+Skip
+Search
+7 kyu
+Remove anchor from URL
+Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+Examples
+"www.codewars.com#about" --> "www.codewars.com"
+"www.codewars.com?page=1" -->"www.codewars.com?page=1"*/
+
+function removeUrlAnchor(url) {
+  return url.split('#')[0];
+}
